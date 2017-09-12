2d8719d0e0fef9a7693cfcb064eccf9a4fa18c82
Paul Goldman <pgoldman0805@gmail.com>
2017-09-12 09:08:27 -0500

    Added controller and view for contact form submissions

--- WeirdEnsemble2/Controllers/ContactController.cs
+++ WeirdEnsemble2/Controllers/ContactController.cs
@@ -1,12 +1,61 @@
-﻿using System.Web.Mvc;
+﻿
+using Microsoft.AspNet.Identity;
+
+using Microsoft.AspNet.Identity.EntityFramework;
+
+using Microsoft.AspNet.Identity.Owin;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Web;
+
+using System.Web.Mvc;
+using WeirdEnsemble2.Models;
 
 namespace WeirdEnsemble2.Controllers
-{
+{ 
     public class ContactController : Controller
     {
+        Entities db = new Entities();
+
+        //Type override and hit tab
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
         // GET: Contact
         public ActionResult Index()
         {
+            return View();
+        }
+
+        //POST: Contact
+        [HttpPost]
+        public ActionResult Index(string fname, string lname, string email, string comment)
+        {
+            if (ModelState.IsValid)
+            {
+                Contact contact = new Contact()
+                {
+                    FirstName = fname,
+                    LastName = lname,
+                    Email = email,
+                    Comment = comment,
+                    DateCreated = DateTime.UtcNow
+                };
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                TempData["ContactConfirmation"] = "Thanks for your comments! We will reply soon.";
+                return RedirectToAction("Index","Home");
+            }
             return View();
         }
     }
--- WeirdEnsemble2/Controllers/ProductController.cs
+++ WeirdEnsemble2/Controllers/ProductController.cs
@@ -122,7 +122,5 @@ namespace WeirdEnsemble2.Controllers
             }
             return View(db.Products.Find(id));
         }
-
-        
     }
 }
